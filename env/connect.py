4abd0ff8edb6be511b83e2ab954d656f0c891159
Nolwenn <duflos.nol@laposte.net>
2023-07-05 09:32:38 +0200

    solution pb

--- env/connect.py
+++ env/connect.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine, text
 
-engine = create_engine("sqlite:///sample.db", echo = True)
+engine = create_engine("postgresql://utilisateur:motdepasse@localhost:port/nom_base_de_donnees", echo = True) #Route vers notre base de données
 
 with engine.connect() as connection:
     result = connection.execute(text('select "Salut !!"'))
